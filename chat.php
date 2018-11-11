a40037ab169ece72fa8bd83d9bdafd7fc5fb4708
AokiTakuto <aokintonto14667@gmail.com>
2018-11-11 14:50:59 +0900

    make login.php

--- chat.php
+++ chat.php
@@ -5,6 +5,7 @@ if (!isset($_SESSION["NAME"])) {
     header("Location: logout.php");
     exit;
 }
+$_SESSION['page']='talks';
 
 
 
@@ -28,36 +29,55 @@ if (!isset($_SESSION["NAME"])) {
                 <!--ステータス-->
                 <div id="bms_chat_user_status">
                     <!--ステータスアイコン-->
-                    <div id="bms_status_icon">●</div>
+                    <div id="bms_status_icon"></div>
                     <!--ユーザー名-->
-                    <div id="bms_chat_user_name">ユーザー</div>
+                    <div id="bms_chat_user_name"></div>
                 </div>
             </div>
 
             <!-- タイムライン部分③ -->
-            <div id="bms_messages">
-                <?php
-                    //<!--メッセージ１（左側）-->
-                    print '<div class="bms_message bms_left">';
-                    print  '<div class="bms_message_box">';
-                    print  '<div class="bms_message_content">';
-                    print  '<div class="bms_message_text">ほうほうこりゃー便利じゃないか</div>';
-                    print  '</div>';
-                    print  '</div>';
-                    print  '</div>';
-                    print  '<div class="bms_clear"></div>';//<!-- 回り込みを解除（スタイルはcssで充てる） -->
+            <?php if ($page == 'talks'): ?>
+                <div id="bms_talks">
+                    <?php
+                        $talks = Array();
+                        $pdo = new PDO ( 'mysql:dbname=chat; host=localhost;port=3306; charset=utf8', 'root', 'Zaq12wsx!' );
+                        $cmd = 'SELECT t_talk.talk_name,t_member.member_name FROM t_auth inner join t_member on t_auth.member_id = t_member.member_id
+                                inner join t_talk on t_talk.talk_id = t_auth.talk_id and t_member.member_id="' .$_SESSION['NAME'] .'";';
+                        foreach($pdo->query($cmd) as $row){
+                            $talks[] = $row['talk_name'];
+                        }
+                        foreach($talks as $talk){
+                            print '<div class="bms_talk_box">';
+                            print '<div>' .$talk .'</div>';
+                            print '</div>';
+                        }
+                    ?>
+                </div>
+            <?php elseif ($page == 'talk'): ?>
+                <div id="bms_messages">
+                    <?php
+                        //<!--メッセージ１（左側）-->
+                        print '<div class="bms_message bms_left">';
+                        print  '<div class="bms_message_box">';
+                        print  '<div class="bms_message_content">';
+                        print  '<div class="bms_message_text">ほうほうこりゃー便利じゃないか</div>';
+                        print  '</div>';
+                        print  '</div>';
+                        print  '</div>';
+                        print  '<div class="bms_clear"></div>';//<!-- 回り込みを解除（スタイルはcssで充てる） -->
 
-                    //<!--メッセージ２（右側）-->
-                    print  '<div class="bms_message bms_right">';
-                    print  '<div class="bms_message_box">';
-                    print  '<div class="bms_message_content">';
-                    print  '<div class="bms_message_text">うん、まあまあいけとるな</div>';
-                    print  '</div>';
-                    print  '</div>';
-                    print  '</div>';
-                    print  '<div class="bms_clear"></div>'; //<!-- 回り込みを解除（スタイルはcssで充てる） -->
-                ?>
-            </div>
+                        //<!--メッセージ２（右側）-->
+                        print  '<div class="bms_message bms_right">';
+                        print  '<div class="bms_message_box">';
+                        print  '<div class="bms_message_content">';
+                        print  '<div class="bms_message_text">うん、まあまあいけとるな</div>';
+                        print  '</div>';
+                        print  '</div>';
+                        print  '</div>';
+                        print  '<div class="bms_clear"></div>'; //<!-- 回り込みを解除（スタイルはcssで充てる） -->
+                    ?>
+                </div>
+            <?php endif;?>
 
             <!-- テキストボックス、送信ボタン④ -->
             <div id="bms_send">
