ce97a617178080e4792216d9c12c4e9a9cc458ce
AokiTakuto <aokintonto14667@gmail.com>
2018-11-11 18:30:34 +0900

    add sent func

--- chat.php
+++ chat.php
@@ -63,9 +63,7 @@ if (isset($_POST["sent"])) {
                     <!--戻るボタン-->
                     <?php if ($_SESSION['page'] == 'talk'): ?>
                         <form action="#" method="POST">
-                        <button type="submit" id="bms_back" name="back">＜</button>
-                        <!--ユーザー名-->
-                        <div><?php print $_SESSION['talk'];?></div>
+                        <button type="submit" id="bms_back" name="back">＜</button><div><?php print $_SESSION['talk'];?></div>
                         </form>
                     <?php endif;?>
                 </div>
